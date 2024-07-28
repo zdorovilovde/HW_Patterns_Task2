d6a3a335635f81f24acba4e03de83b2694a3efed
zdorovilov test <zdorovilovde@mail.ru>
2024-07-28 15:08:14 +0500

    Update AutoTest

--- src/test/java/ru/netology/test/AutoTest.java
+++ src/test/java/ru/netology/test/AutoTest.java
@@ -1,4 +1,13 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.nio.channels.ConnectionPendingException;
+
 public class AutoTest {
+
+    @BeforeEach
+    void setup() {
+        open()
+    }
 }
