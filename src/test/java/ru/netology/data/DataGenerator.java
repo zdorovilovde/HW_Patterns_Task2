0b1571a4a887108b1a11c4b0218969021d8a8299
zdorovilov test <zdorovilovde@mail.ru>
2024-07-28 15:10:32 +0500

    Update DataGenerator

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,4 +1,42 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
 public class DataGenerator {
+
+    private static final Faker FAKER = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    public static String getRandomLogin() {
+        return FAKER.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return FAKER.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+    }
+
+    public static RegistrationDto getUser(String status) {
+        return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+    }
+
+    public static RegistrationDto getRegistrationUser(String status) {
+        return ApiHelper.sendRequest(getUser(status));
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
 }
