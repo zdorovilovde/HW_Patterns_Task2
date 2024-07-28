cf13bbe16440f78639c1604ef1f023fd51585d3f
zdorovilov test <zdorovilovde@mail.ru>
2024-07-28 15:44:17 +0500

    new Update AutoTest

--- src/test/java/ru/netology/test/AutoTest.java
+++ src/test/java/ru/netology/test/AutoTest.java
@@ -1,13 +1,28 @@
 package ru.netology.test;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.nio.channels.ConnectionPendingException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class AutoTest {
 
     @BeforeEach
     void setup() {
-        open()
+        open("http://localhost:9999/");
     }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfullyLoginWithActiveRegisteredUser(){}
+    //    var registeredUser = getRegisteredUser("active");
+    //    $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+    //    $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+    //}
+
+
 }
