dd94f8b9974f76a68d20c41ece80eaf9379a9865
zdorovilov test <zdorovilovde@mail.ru>
2024-07-28 14:59:57 +0500

    Вспомогательный класс отправки запроса на сервер

---
+++ src/test/java/ru/netology/data/ApiHelper.java
@@ -0,0 +1,32 @@
+package ru.netology.data;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private ApiHelper() {
+    }
+
+    static DataGenerator.RegistrationDto sendRequest(DataGenerator.RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user).when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+        return user;
+    }
+}
